1fd6903220a22f7cdf8f6ac8c133d6986096d8f4
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-22 17:54:07 +0300

    Add user login endpoint with TokenObtainPairView

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from account.views import UserCreateApiView
 
 urlpatterns = [
     path("register/", UserCreateApiView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
 ]
 
 app_name = "account"
