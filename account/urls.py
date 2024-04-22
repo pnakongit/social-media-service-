83ac591a69824fa6078175804318660b7b9047d2
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-22 17:57:37 +0300

    Add token refresh endpoind with TokenRefreshView in account app

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from account.views import UserCreateApiView
 
 urlpatterns = [
     path("register/", UserCreateApiView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh")
 ]
 
 app_name = "account"
