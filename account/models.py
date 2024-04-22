e5bfb63375993d48c59e6161cffe0f683023b27f
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-22 15:53:46 +0300

    Implement User model with custom fields and email as username field

--- account/models.py
+++ account/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from helpers import upload_image_file_path
+from account.managers import UserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    bio = models.CharField(max_length=255, blank=True, null=True)
+    picture = models.ImageField(
+        upload_to=upload_image_file_path,
+        blank=True,
+        null=True
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.email}"
